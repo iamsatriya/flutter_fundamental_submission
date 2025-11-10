a1faa331f6af236bbfd9d0a486a97ec0d911fecb
Satriya Wicaksana <satriya.wicaksana@sinarmasmining.com>
2025-11-10 11:13:56 +0700

    feat: fetch data in detail screen

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:new_fundamental_submission/provider/detail/restaurant_detail_provider.dart';
+import 'package:new_fundamental_submission/static/state/restaurant_detail_result_state.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String restaurantId;
   const DetailScreen({super.key, required this.restaurantId});
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      if (!mounted) return;
+      context.read<RestaurantDetailProvider>().fetchRestaurantDetail(
+        widget.restaurantId,
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Detail Screen of $restaurantId')),
+      body: SafeArea(
+        child: Consumer<RestaurantDetailProvider>(
+          builder: (context, value, child) {
+            return switch (value.resultState) {
+              RestaurantDetailSuccessState(:final data) => Column(
+                children: [Text(data.name), Text(data.description)],
+              ),
+              RestaurantDetailErrorState(:final error) => Center(
+                child: Text(error),
+              ),
+              RestaurantDetailLoadingState() => const Center(
+                child: CircularProgressIndicator(),
+              ),
+              _ => const SizedBox(),
+            };
+          },
+        ),
+      ),
     );
   }
 }

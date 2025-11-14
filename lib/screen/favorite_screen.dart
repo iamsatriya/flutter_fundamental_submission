761953489b951e6e4342ecf910cfd52bb478729d
Satriya Wicaksana <satriya.wicaksana888@gmail.com>
2025-11-14 09:39:19 +0700

    feat: styling favorite screen

--- lib/screen/favorite_screen.dart
+++ lib/screen/favorite_screen.dart
@@ -11,45 +11,52 @@ class FavoriteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Favorite Restaurant'),
-            Expanded(
-              child: Consumer<LocalDatabaseProvider>(
-                builder: (context, value, child) {
-                  if (value.favoriteRestaurantList == null) {
-                    return const SizedBox();
-                  }
-                  final restaurantList = value.favoriteRestaurantList!;
-                  return switch (restaurantList.length) {
-                    0 => const Center(
-                      child: Text('There are no favorite restaurant found.'),
-                    ),
-                    _ => ListView.separated(
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(height: 8),
-                      itemCount: restaurantList.length,
-                      itemBuilder: (context, index) {
-                        final restaurant = restaurantList[index];
-                        return ListTile(
-                          title: Text(restaurant.name),
-                          subtitle: Text(restaurant.city),
-                          trailing: Favorite(restaurant: restaurant),
-                          onTap: () {
-                            Navigator.pushNamed(
-                              context,
-                              NavigationRoute.detail.name,
-                              arguments: restaurant.id,
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  };
-                },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Favorite Restaurant',
+                style: Theme.of(context).textTheme.headlineMedium,
               ),
-            ),
-          ],
+              Expanded(
+                child: Consumer<LocalDatabaseProvider>(
+                  builder: (context, value, child) {
+                    if (value.favoriteRestaurantList == null) {
+                      return const SizedBox();
+                    }
+                    final restaurantList = value.favoriteRestaurantList!;
+                    return switch (restaurantList.length) {
+                      0 => const Center(
+                        child: Text('There are no favorite restaurant found.'),
+                      ),
+                      _ => ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(height: 8),
+                        itemCount: restaurantList.length,
+                        itemBuilder: (context, index) {
+                          final restaurant = restaurantList[index];
+                          return ListTile(
+                            title: Text(restaurant.name),
+                            subtitle: Text(restaurant.city),
+                            trailing: Favorite(restaurant: restaurant),
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                NavigationRoute.detail.name,
+                                arguments: restaurant.id,
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    };
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
